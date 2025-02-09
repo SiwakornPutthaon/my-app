d5f581f14349703cc3b86ddd92e217ab8d906b65
Siwakorn <siwakornputtanon@gmail.com>
2025-02-09 15:41:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,4 @@
 import streamlit as st
-from PIL import Image
-
-try:
-    img = Image.open("images/S__90898435.jpg")
-    st.image(img)
-except Exception as e:
-    st.error(f"Error loading image: {e}")
   
 st.title('🎈 Siwakorn Putthanon, BDA')
 st.info('Business Data Analyst with KPI development, dashboard design, and customer behavior analysis', icon="ℹ️")
