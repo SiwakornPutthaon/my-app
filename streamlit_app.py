ccbca2ba8e7bbdc3bf314d828132a08e1cb4a237
Siwakorn <siwakornputtanon@gmail.com>
2025-02-09 15:28:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
-st.title('🎈 Siwakorn Putthanon')
+st.image("C:\Users\siwak\Desktop\สอบ\picture\S__90898435.jpg")
+st.title('🎈 Siwakorn Putthanon, BDA')
+st.info('Business Data Analyst with in KPI development, dashboard design, and customer behavior analysis', icon="ℹ️")
+
 
 st.write('Hello world!')
